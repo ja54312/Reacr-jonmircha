88003ea6aa9480ac2eebf3c1cd4982f16c7c7aa0
ja54312 <ija54312@gmail.com>
2022-01-25 13:20:50 -0600

    26REact-bootstrap && Material UI

--- React-FrameworkCSS/src/App.js
+++ React-FrameworkCSS/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import Bootstrap from './components/Bootstrap';
+import MaterialUI from './components/MaterialUI';
+// import ReactBootstrap from './components/react-bootstap';
 
 function App() {
   return (
     <div className="App">
       <h1>Framework CSS con React</h1>
-      <Bootstrap/>
+      {/* <ReactBootstrap/> */}
+      <MaterialUI/>
     </div>
   );
 }
---
+++ React-FrameworkCSS/src/components/MaterialUI.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MediaCard from "./MaterialCard";
+import PersistentDrawerLeft from "./MaterialDrawer";
+
+export default function MaterialUI(){
+    return(
+        <>
+        <h2>MaterialUI</h2>
+        <MediaCard/>
+        <PersistentDrawerLeft/>
+        </>
+    )
+}
