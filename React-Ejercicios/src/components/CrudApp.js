cf0aa96ee6b002bcc7e8931e55d6fc04f99c98af
ja54312 <ija54312@gmail.com>
2022-10-15 19:19:55 -0500

    crud 4

--- React-Ejercicios/src/components/CrudApp.js
+++ React-Ejercicios/src/components/CrudApp.js
@@ -42,22 +42,32 @@ const CrudApp = () => {
     let newData = db.map(el => el.id === data.id?data:el)
     setDb(newData)
   };
-  const deleteData = (id) => {};
+  const deleteData = (id) => {
+    let isDelete = window.confirm(`¿Estas seguro de eliminar el registro con el ${id}?`)
+      if (isDelete) {
+        let newData = db.filter((el) => el.id !== id);
+        setDb(newData);
+      } else {
+        return;
+      }
+  };
 
   return (
     <div>
       <div>CrudApp</div>
-      <CrudForm
-        createData={createData}
-        updateData={updateData}
-        datatoEdit={datatoEdit}
-        setDataToEdit={setDataToEdit}
-      />
-      <CrudTable
-        data={db}
-        setDataToEdit={setDataToEdit}
-        deleteData={deleteData}
-      />
+      <article className="grid-1-2">
+        <CrudForm
+          createData={createData}
+          updateData={updateData}
+          datatoEdit={datatoEdit}
+          setDataToEdit={setDataToEdit}
+        />
+        <CrudTable
+          data={db}
+          setDataToEdit={setDataToEdit}
+          deleteData={deleteData}
+        />
+      </article>
     </div>
   );
 };
