a72a288f20b47ceec76a67c36b2a798e1458cd18
ja54312 <ija54312@gmail.com>
2022-10-25 14:09:42 -0500

    loader and mesage

--- React-Ejercicios/src/components/CrudApi.js
+++ React-Ejercicios/src/components/CrudApi.js
@@ -2,41 +2,50 @@ import React, { useEffect, useState } from "react";
 import { helpHttp } from "../helpers/helpHttp";
 import CrudForm from "./CrudForm";
 import CrudTable from "./CrudTable";
+import Loader from "./Loader";
+import Message from "./Message";
 
 const CrudApi = () => {
-  const [db, setDb] = useState([]);
+  const [db, setDb] = useState(null);
   const [datatoEdit, setDataToEdit] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-  let api = helpHttp()
-  let url = "http://localhost:5000/santos"
+
+  let url = "http://localhost:5000/santos";
 
   useEffect(() => {
-    api.get(url).then((res)=>{    
-      if(!res.err){
-        setDb(res)
-      }else{
-        setDb([])
+    setLoading(true);
+    helpHttp().get(url).then((res) => {
+      if (!res.err) {
+        setDb(res);
+        setError(null);
+      } else {
+        setDb(null);
+        setError(res);
       }
-    })
-  },[])
-  
+    });
+    setLoading(false);
+  }, [url]);
 
   const createData = (data) => {
     data.id = Date.now();
     setDb([...db, data]);
   };
   const updateData = (data) => {
-    let newData = db.map(el => el.id === data.id?data:el)
-    setDb(newData)
+    let newData = db.map((el) => (el.id === data.id ? data : el));
+    setDb(newData);
   };
   const deleteData = (id) => {
-    let isDelete = window.confirm(`¿Estas seguro de eliminar el registro con el ${id}?`)
-      if (isDelete) {
-        let newData = db.filter((el) => el.id !== id);
-        setDb(newData);
-      } else {
-        return;
-      }
+    let isDelete = window.confirm(
+      `¿Estas seguro de eliminar el registro con el ${id}?`
+    );
+    if (isDelete) {
+      let newData = db.filter((el) => el.id !== id);
+      setDb(newData);
+    } else {
+      return;
+    }
   };
 
   return (
@@ -49,11 +58,20 @@ const CrudApi = () => {
           datatoEdit={datatoEdit}
           setDataToEdit={setDataToEdit}
         />
-        <CrudTable
-          data={db}
-          setDataToEdit={setDataToEdit}
-          deleteData={deleteData}
-        />
+        {loading && <Loader />}
+        {error && (
+          <Message
+            msg={`Error ${error.status}: ${error.statusText}`}
+            bgColor="#dc3545"
+          />
+        )}
+        {db && (
+          <CrudTable
+            data={db}
+            setDataToEdit={setDataToEdit}
+            deleteData={deleteData}
+          />
+        )}
       </article>
     </div>
   );
